979956d2c6c3047038ca44503ba7d6d7344bc8ef
Tim Sandquist <tsndqst@users.noreply.github.com>
2020-05-22 20:44:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
 resource "null_resource" "example" {
 }
-resource "null_resource" "example2" {
-garbage
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 }
+
+# Create NGINX container
+resource "docker_container" "nginx" {
+  image = "\${docker_image.nginx.latest}"
+  name  = "enginecks"
+  ports {
+    internal = 80
+    external = 80
+  }
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+EOF
