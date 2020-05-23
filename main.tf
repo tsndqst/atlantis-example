c59d4bf6b7955b467dd541ce695bfba321d9ea37
Tim Sandquist <tsndqst@users.noreply.github.com>
2020-05-22 20:56:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,16 +5,6 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-# Create NGINX container
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "enginecks"
-  ports {
-    internal = 80
-    external = 8888
-  }
-}
-
 resource "docker_image" "nginx" {
   name = "nginx:latest"
 }
